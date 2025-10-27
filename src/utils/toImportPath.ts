2671d0ba01ec7cf552ef16709ea621b71cd46e39
axtk <axtkx@inbox.ru>
2025-10-27 19:39:26 +0300

    fix import paths

--- src/utils/toImportPath.ts
+++ src/utils/toImportPath.ts
@@ -2,9 +2,9 @@ import { join, posix, relative, sep } from "node:path";
 
 export function toImportPath(relativePath: string, referencePath = ".") {
   let cwd = process.cwd();
-  let importPath = posix.join(
-    ...relative(join(cwd, referencePath), relativePath).split(sep),
-  );
+  let importPath = posix
+    .join(...relative(join(cwd, referencePath), relativePath).split(sep))
+    .replace(/\.tsx?$/, ".js");
 
   if (importPath && !/^\.+\//.test(importPath)) importPath = `./${importPath}`;
 
