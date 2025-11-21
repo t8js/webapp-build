efd9717d4e5f98eef8a85537ae33e65557ec46eb
axtk <axtkx@inbox.ru>
2025-11-21 16:50:37 +0300

    get rid of waitFor with build output

--- src/build.ts
+++ src/build.ts
@@ -4,12 +4,15 @@ import type { BuildParams } from "./types/BuildParams.ts";
 import { buildClient } from "./utils/buildClient.ts";
 import { buildServer } from "./utils/buildServer.ts";
 import { createPostbuildPlugins } from "./utils/createPostbuildPlugins.ts";
-import { waitFor } from "./utils/waitFor.ts";
 
 export async function build(params: BuildParams) {
   let startTime = Date.now();
   let log = params.silent ? () => {} : console.log;
+
+  log("Build started");
+
   let serverProcess: ChildProcess | null = null;
+  let inited = false;
 
   function startServer() {
     if (serverProcess) {
@@ -17,27 +20,18 @@ export async function build(params: BuildParams) {
       serverProcess = null;
     }
 
-    let serverPath = `${params.targetDir}/server/index.js`;
+    if (!inited) {
+      log(`Build completed +${formatDuration(Date.now() - startTime)}`);
+      inited = true;
+    }
 
-    waitFor(serverPath)
-      .then(() => {
-        serverProcess = spawn("node", [serverPath], { stdio: "inherit" });
-      })
-      .catch(() => {
-        log(`File not found: "${serverPath}"`);
-        process.exit(1);
-      });
+    serverProcess = spawn("node", [`${params.targetDir}/server/index.js`], { stdio: "inherit" });
   }
 
   let { serverPlugins } = createPostbuildPlugins(startServer);
-
-  log("Build started");
 
   await Promise.all([
     buildServer(params, serverPlugins),
     buildClient(params),
   ]);
-
-  log(`Build completed +${formatDuration(Date.now() - startTime)}`);
-  startServer();
 }
